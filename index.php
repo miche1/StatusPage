c61f8a256ba6bcd08fd0b94a327cd5f2d0e9354c
Michél Popp <michel_popp@web.de>
2017-03-07 19:04:39 +0100

    modify index.php -- Design

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <html>
 <head>
   <title>Status</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="bootstrap.css">
 </head>
-<body>
-  <h1>Status</h1>
-  <h2>Hosts</h2>	
-  <?php
-    
- $logFile = file_get_contents('/home/monitor/status-log.txt');
+<body style="background-color: #ecf0f1;">
 
+  <div class="jumbotron" style="background-color: #ecf0f1; width:80%; margin: auto; padding-top: 2em;">
+    <div style="width:90%;margin: auto;">
+    <h1><b>status</b></h1>
+    <h2>services</h2>
+    <br>
+
+  <?php
+
+  $logFile = file_get_contents('/home/monitor/status-log.txt');
 
     $data = explode( ';', $logFile);
+
 
   status($data[1],"VPN");
   status($data[3],"cloud");
@@ -19,14 +26,18 @@
 	{
 	    if ($stat == "true")
 	    {
-   		 print("<h3>".$host." -- online </h3>");
+   		 print('<div class="alert alert-success" role="alert"><h4><span class="label label-default">online</span>&emsp;'.$host.'</h4></div>');
    	    }
    	    else
-    	    {
-	    print("<h3>".$host." -- offline </h3>");
+    	{
+	    print('<div class="alert alert-danger" role="alert"><h4><span class="label label-default">offline</span>&emsp;'.$host.'</h4></div>');
 	    }
 	}
+
   ?>
 
+  <button class="btn btn-default" href="#">show logs</button>
+</div>
+</div>
 </body>
 </html>
