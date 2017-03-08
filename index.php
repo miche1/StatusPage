5624c98eb9deb275f16c9155f236ddbb9c77877e
Michél Popp <michel_popp@web.de>
2017-03-08 17:26:49 +0100

    update index.php -- webapp settings

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
 <head>
-  <title>monitoring</title>
+  <title>status</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="stylesheet" href="bootstrap.css">
+
+  <!--webapp settings-->
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <!--<link rel="manifest" href="manifest.json">-->
+  <meta name="home.control" content="home.control">
+
 </head>
 <body style="background-color: #ecf0f1;">
 
