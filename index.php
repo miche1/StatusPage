7a9cbf149b92d07228b0814185f3d357d627f77f
Michél Popp <michel_popp@web.de>
2017-03-08 18:14:32 +0100

    add icon && add refresh button && readme file

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
 <head>
   <title>status</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link rel="stylesheet" href="bootstrap.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
   <!--webapp settings-->
+  <link rel="apple-touch-icon" href="media/icon.png"/>
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <!--<link rel="manifest" href="manifest.json">-->
-  <meta name="home.control" content="home.control">
+  <meta name="status" content="status">
 
 </head>
 <body style="background-color: #ecf0f1;">
 
+<form method="get" action="index.php">
   <div class="jumbotron" style="background-color: #ecf0f1; width:80%; margin: auto; padding-top: 2em;">
     <div style="width:90%;margin: auto;">
     <h1><b>STATUS</b></h1>
-    <h2>services</h2>
+    <h2>services
+      <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-refresh"></span> Refresh</button>
+    </h2>
     <br>
 
   <?php
@@ -46,5 +50,6 @@
   <button class="btn btn-default" href="#">show logs</button>
 </div>
 </div>
+</form="">
 </body>
 </html>
